1bb77d7b4e85e3d42cd058db96a677ec1c222edd
Lantonium <49911858+lantonium@users.noreply.github.com>
2022-11-25 22:58:58 -0500

    Update Sprint 3 | Fixed Errors | Navigation + Pages

--- lib/pages/piramegipto_page.dart
+++ lib/pages/piramegipto_page.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:Androidlab/pages/menu_page.dart';
 
 
-
-class PoiPage extends StatefulWidget {
-  const PoiPage({Key? key}) : super(key: key);
+class PiramegiptoPage extends StatefulWidget {
+  const PiramegiptoPage({Key? key}) : super(key: key);
 
   @override
-  _PoiPageState createState() => _PoiPageState();
+  _PiramegiptoPageState createState() => _PiramegiptoPageState();
 }
 
-class _PoiPageState extends State<PoiPage> {
+class _PiramegiptoPageState extends State<PiramegiptoPage> {
   void initState() {
     //pasarPage();
     super.initState();
@@ -33,40 +32,29 @@ class _PoiPageState extends State<PoiPage> {
           title: const Text('Detalle Sitio Turistico'),
         ),
         body: Center(
+          child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
               const SizedBox(height: 15),
-              const Text('Machu Picchu', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              const Text('Piramides de Egipto', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
               const SizedBox(height: 30),
-              const Image(image: AssetImage("assets/images/machupicchu.png")),
+              const Image(image: AssetImage("assets/images/piramegipto.jpg")),
               const SizedBox(height: 15),
-              const Text('Horario: 07:00 a 18:00 Horas',
+              const Text('Horario: 24 Horas',
                   textAlign: TextAlign.left, style: TextStyle(fontSize: 20)),
               const SizedBox(height: 10),
               const Text(
-                  'Temperatura: La ciudad Inca de Machu Picchu se encuentra'
-                  'a 2400 m.s.n.m. teniendo un clima cálido y húmedo, '
-                  'con temperaturas entre los 8° y 22°C.  '),
+                  'Temperatura: El clima de Egipto tiene dos estaciones, un invierno templado entre noviembre y abril y un verano cálido y seco de mayo a octubre. El país es en gran parte desértico, solo en el estrecho cinturón cruzado por el río Nilo son posibles los cultivos.  '),
               const SizedBox(height: 10),
               const Text(
-                  'Direccion: Cusco (provincia de Urubamba, distrito '
-                  'de Machu Picchu) sobre el Valle Sagrado de los Incas,'
-                  ' a 80 kilómetros al noroeste de la ciudad de Cusco.'),
+                  'Direccion: Están ubicadas cerca de El Cairo, las más alejadas a 60 kilómetros de la ciudad, y más bien al sur. Están todos en la orilla izquierda del Nilo, hacia el oeste. .'),
               const SizedBox(height: 15),
               const Text(
-                  'Descripcion: Machu Picchu es una ciudadela inca '
-                  'ubicada en las alturas de las montañas '
-                  'de los Andes en Perú, sobre el valle del '
-                  'río Urubamba.'),
+                  'Descripcion: Las pirámides de Egipto son, de todos los vestigios legados por los egipcios de la Antigüedad,'
+                      ' los más portentosos y emblemáticos reconocidos. '),
               const SizedBox(height: 15),
               const Text(
-                  'Se construyó en el siglo XV '
-                  'y luego fue abandonada, y es famosa por '
-                  'sus sofisticadas paredes de piedra seca que combinan '
-                  'enormes bloques sin el uso de un mortero, los edificios '
-                  'fascinantes que se relacionan con las alineaciones '
-                  'astronómicas y sus vistas panorámicas. '
-                  'El uso exacto que tuvo sigue siendo un misterio.',
+                  'Construidas como criptas reales para los faraones a partir de 2630–2610 a. C.1​, con bloques de piedra revestidos de caliza, eran a la vista, grandes construcciones de color blanco.',
                   style: TextStyle(fontSize: 14)),
               const SizedBox(height: 15),
 
@@ -82,7 +70,8 @@ class _PoiPageState extends State<PoiPage> {
             ],
           ),
         ),
-      ),
+          ),
+    ),
     );
   }
 }
--- lib/pages/tajmahal_page.dart
+++ lib/pages/tajmahal_page.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:Androidlab/pages/menu_page.dart';
 
 
-
-class PoiPage extends StatefulWidget {
-  const PoiPage({Key? key}) : super(key: key);
+class TajmahalPage extends StatefulWidget {
+  const TajmahalPage({Key? key}) : super(key: key);
 
   @override
-  _PoiPageState createState() => _PoiPageState();
+  _TajmahalPageState createState() => _TajmahalPageState();
 }
 
-class _PoiPageState extends State<PoiPage> {
+class _TajmahalPageState extends State<TajmahalPage> {
   void initState() {
     //pasarPage();
     super.initState();
@@ -33,40 +32,32 @@ class _PoiPageState extends State<PoiPage> {
           title: const Text('Detalle Sitio Turistico'),
         ),
         body: Center(
+          child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
               const SizedBox(height: 15),
-              const Text('Machu Picchu', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              const Text('Taj-Mahal', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
               const SizedBox(height: 30),
-              const Image(image: AssetImage("assets/images/machupicchu.png")),
+              const Image(image: AssetImage("assets/images/taj-mahal.jpg")),
               const SizedBox(height: 15),
-              const Text('Horario: 07:00 a 18:00 Horas',
+              const Text('Horario: 09:00 a 16:00 Horas',
                   textAlign: TextAlign.left, style: TextStyle(fontSize: 20)),
               const SizedBox(height: 10),
               const Text(
-                  'Temperatura: La ciudad Inca de Machu Picchu se encuentra'
-                  'a 2400 m.s.n.m. teniendo un clima cálido y húmedo, '
-                  'con temperaturas entre los 8° y 22°C.  '),
+                  'Temperatura: El clima del Taj Mahal y Agra tiene tres estaciones distintas con solo una que es favorable para turistas y visitantes. Las tres estaciones son el invierno suave, el verano increíblemente caluroso y el monzón húmedo. La mejor temporada para visitar el Taj Mahal es'
+                      'durante los meses de invierno de noviembre a febrero. '),
               const SizedBox(height: 10),
               const Text(
-                  'Direccion: Cusco (provincia de Urubamba, distrito '
-                  'de Machu Picchu) sobre el Valle Sagrado de los Incas,'
-                  ' a 80 kilómetros al noroeste de la ciudad de Cusco.'),
+                  'Direccion: Taj Mahal se encuentra en la ciudad india de Agra aproximadamente '
+                      'a 200 km al sur de Delhi, la capital de la India..'),
               const SizedBox(height: 15),
               const Text(
-                  'Descripcion: Machu Picchu es una ciudadela inca '
-                  'ubicada en las alturas de las montañas '
-                  'de los Andes en Perú, sobre el valle del '
-                  'río Urubamba.'),
+                  'Descripcion: es un monumento funerario construido entre 1632 y 1648 en la ciudad de Agra, estado de Uttar Pradesh (India), a orillas del río Yamuna, por el emperador musulmán Shah Jahan de la dinastía mogol. El imponente conjunto de edificios se erigió en honor de su esposa favorita, Arjumand Banu Begum —más conocida como Mumtaz Mahal— que '
+                      'murió en el parto de su decimocuarto hijo'),
               const SizedBox(height: 15),
               const Text(
-                  'Se construyó en el siglo XV '
-                  'y luego fue abandonada, y es famosa por '
-                  'sus sofisticadas paredes de piedra seca que combinan '
-                  'enormes bloques sin el uso de un mortero, los edificios '
-                  'fascinantes que se relacionan con las alineaciones '
-                  'astronómicas y sus vistas panorámicas. '
-                  'El uso exacto que tuvo sigue siendo un misterio.',
+                  'Se estima que en su construcción trabajaron unos 20 000 obreros bajo dirección de un conjunto de arquitectos liderados por el arquitecto de la corte, Ustad Ahmad Lahori.'
+                  '',
                   style: TextStyle(fontSize: 14)),
               const SizedBox(height: 15),
 
@@ -83,6 +74,7 @@ class _PoiPageState extends State<PoiPage> {
           ),
         ),
       ),
+    ),
     );
   }
 }
